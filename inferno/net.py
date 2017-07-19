f5bb3c9a0ad9c0875e3d0ffcd8480be8bff57a05
Benjamin Bossan <benjamin.bossan@ottogroup.com>
2017-07-19 13:45:44 +0200

    Move some functions to utils.py.

--- inferno/net.py
+++ inferno/net.py
@@ -5,7 +5,6 @@ import pickle
 import numpy as np
 from sklearn.base import BaseEstimator
 import torch
-from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
@@ -14,46 +13,10 @@ from inferno.callbacks import AverageLoss
 from inferno.callbacks import BestLoss
 from inferno.callbacks import Callback
 from inferno.callbacks import EpochTimer
-
-
-def to_var(X, use_cuda=False):
-    X = to_tensor(X, use_cuda=use_cuda)
-    return Variable(X)
-
-
-def to_tensor(X, use_cuda=False):
-    """Turn to torch Variable.
-
-    Handles the cases:
-      * Variable
-      * PackedSequence
-      * dict
-      * numpy array
-      * torch Tensor
-
-    """
-    if isinstance(X, (Variable, nn.utils.rnn.PackedSequence)):
-        return X
-
-    if isinstance(X, dict):
-        return {key: to_tensor(val) for key, val in X.items()}
-
-    if isinstance(X, np.ndarray):
-        X = torch.from_numpy(X)
-
-    if use_cuda:
-        X = X.cuda()
-    return X
-
-
-def to_numpy(X):
-    if X.is_cuda:
-        X = X.cpu()
-    try:
-        data = X.data
-    except AttributeError:
-        data = X
-    return data.numpy()
+from inferno.callbacks import Scoring
+from inferno.utils import to_numpy
+from inferno.utils import to_tensor
+from inferno.utils import to_var
 
 
 class History(list):
