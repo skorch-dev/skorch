1b17fb232df77256ce7e9e429b83a8f239626e30
Benjamin Bossan <benjamin.bossan@ottogroup.com>
2017-07-20 16:05:45 +0200

    Add tests for BestLoss callback.

--- inferno/tests/test_callbacks.py
+++ inferno/tests/test_callbacks.py
@@ -14,7 +14,7 @@ class TestAverageLoss:
 
     @pytest.fixture
     def avg_loss(self, avg_loss_cls):
-        return avg_loss_cls()
+        return avg_loss_cls().initialize()
 
     @pytest.fixture
     def history_avg_loss(self, avg_loss):
@@ -57,9 +57,9 @@ class TestAverageLoss:
 
         assert history[0, 'train_loss'] == 30
 
-    def test_init_ignore_keys(self, avg_loss_cls):
+    def test_init_other_keys(self, avg_loss_cls):
         avg_loss = avg_loss_cls(keys_possible=[
-            ('train_loss', 'train_batch_size')])
+            ('train_loss', 'train_batch_size')]).initialize()
         history = get_history(avg_loss)
 
         train_losses = history[:, 'train_loss']
@@ -68,3 +68,58 @@ class TestAverageLoss:
 
         with pytest.raises(KeyError):
             history[:, 'valid_loss']
+
+
+class TestBestLoss:
+    @pytest.fixture
+    def avg_loss(self):
+        from inferno.callbacks import AverageLoss
+        return AverageLoss()
+
+    @pytest.fixture
+    def best_loss_cls(self):
+        from inferno.callbacks import BestLoss
+        return BestLoss
+
+    @pytest.fixture
+    def best_loss(self, best_loss_cls):
+        return best_loss_cls(signs=(-1, 1)).initialize()
+
+    @pytest.fixture
+    def history_best_loss(self, avg_loss, best_loss):
+        return get_history(avg_loss, best_loss)
+
+    def test_best_loss_correct(self, history_best_loss):
+        train_loss_best = history_best_loss[:, 'train_loss_best']
+        expected = [True, False, True]
+        assert train_loss_best == expected
+
+        valid_loss_best = history_best_loss[:, 'valid_loss_best']
+        expected = [True, False, True]
+        assert valid_loss_best == expected
+
+    def test_other_signs(self, best_loss_cls, avg_loss):
+        best_loss = best_loss_cls(signs=(1, -1)).initialize()
+        history = get_history(avg_loss, best_loss)
+
+        train_loss_best = history[:, 'train_loss_best']
+        expected = [True, True, False]
+        assert train_loss_best == expected
+
+        valid_loss_best = history[:, 'valid_loss_best']
+        expected = [True, True, False]
+        assert valid_loss_best == expected
+
+    def test_init_other_keys(self, best_loss_cls, avg_loss):
+        best_loss = best_loss_cls(
+            keys_possible=('valid_loss',),
+            signs=(1,),
+        ).initialize()
+        history = get_history(avg_loss, best_loss)
+
+        with pytest.raises(KeyError):
+            history[:, 'train_loss_best']
+
+        valid_loss_best = history[:, 'valid_loss_best']
+        expected = [True, False, True]
+        assert valid_loss_best == expected
