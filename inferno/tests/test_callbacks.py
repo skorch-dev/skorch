d5fb9500db1888178acf0d8a3706afb1d3d82426
Benjamin Bossan <benjamin.bossan@ottogroup.com>
2017-07-20 17:31:24 +0200

    Add tests for Scoring callback.

--- inferno/tests/test_callbacks.py
+++ inferno/tests/test_callbacks.py
@@ -1,4 +1,6 @@
+from functools import partial
 from unittest.mock import Mock
+from unittest.mock import patch
 
 import numpy as np
 import pytest
@@ -123,3 +125,131 @@ class TestBestLoss:
         valid_loss_best = history[:, 'valid_loss_best']
         expected = [True, False, True]
         assert valid_loss_best == expected
+
+
+class TestScoring:
+    @pytest.yield_fixture
+    def scoring_cls(self):
+        with patch('inferno.callbacks.to_var') as to_var:
+            to_var.side_effect = lambda x: x
+
+            from inferno.callbacks import Scoring
+            yield partial(
+                Scoring,
+                target_extractor=Mock(side_effect=lambda x: x),
+                pred_extractor=Mock(side_effect=lambda x: x),
+            )
+
+    @pytest.fixture
+    def mse_scoring(self, scoring_cls):
+        return scoring_cls(
+            name='mse',
+            scoring='mean_squared_error',
+        )
+
+    @pytest.fixture
+    def net(self):
+        from inferno.net import History
+
+        net = Mock(module_=Mock(side_effect=lambda x: x))
+        history = History()
+        history.new_epoch()
+        net.history = history
+        return net
+
+    @pytest.fixture
+    def data(self):
+        return [
+            [[3, -2.5], [6, 1.5]],
+            [[1, 0], [0, -1]],
+        ]
+
+    @pytest.fixture
+    def history(self, mse_scoring, net, data):
+        for x, y in data:
+            net.history.new_batch()
+            mse_scoring.on_batch_end(net, x, y, train=False)
+        return net.history
+
+    def test_correct_mse(self, history):
+        mse = history[:, 'batches', :, 'mse']
+        expected = [[12.5, 1.0]]
+        assert np.allclose(mse, expected)
+
+    def test_other_score_and_name(self, scoring_cls, net):
+        scoring = scoring_cls(
+            name='acc',
+            scoring='accuracy_score',
+        )
+        for x, y in zip(np.arange(5), np.arange(5)[::-1]):
+            net.history.new_batch()
+            scoring.on_batch_end(net, [x], [y], train=False)
+
+        acc = net.history[:, 'batches', :, 'acc']
+        expected = [0.0, 0.0, 1.0, 0.0, 0.0]
+        assert np.allclose(acc, expected)
+
+    def test_custom_scoring_func(self, scoring_cls, net):
+        def score_func(estimator, X, y):
+            return 555
+
+        scoring = scoring_cls(
+            name='acc',
+            scoring=score_func,
+        )
+        for x, y in zip(np.arange(5), np.arange(5)[::-1]):
+            net.history.new_batch()
+            scoring.on_batch_end(net, [x], [y], train=False)
+
+        acc = net.history[:, 'batches', :, 'acc']
+        expected = [555] * 5
+        assert np.allclose(acc, expected)
+
+    def test_score_func_does_not_exist(self, scoring_cls, net, data):
+        scoring = scoring_cls(
+            name='myscore',
+            scoring='nonexistant-score',
+        )
+        with pytest.raises(NameError) as exc:
+            net.history.new_batch()
+            scoring.on_batch_end(net, data[0][0], data[0][1], train=False)
+
+        expected = ("Metric with name 'nonexistant-score' does not exist, "
+                    "use a valid sklearn metric name.")
+        assert str(exc.value) == expected
+
+    def test_train_is_ignored(self, mse_scoring, net, data):
+        for x, y in data:
+            net.history.new_batch()
+            mse_scoring.on_batch_end(net, x, y, train=True)
+
+        with pytest.raises(KeyError):
+            net.history[:, 'batches', :, 'mse']
+
+    def test_valid_is_ignored(self, scoring_cls, net, data):
+        mse_scoring = scoring_cls(
+            name='mse',
+            scoring='mean_squared_error',
+            on_train=True,
+        )
+
+        for x, y in data:
+            net.history.new_batch()
+            mse_scoring.on_batch_end(net, x, y, train=False)
+
+        with pytest.raises(KeyError):
+            net.history[:, 'batches', :, 'mse']
+
+    def test_target_extractor_is_called(self, mse_scoring, data, history):
+        # note: the history fixture is required even if not used because it
+        # triggers the calls on mse_scoring
+        call_args_list = mse_scoring.target_extractor.call_args_list
+        for (_, x), call_args in zip(data, call_args_list):
+            assert x == call_args[0][0]
+
+    def test_pred_extractor_is_called(self, mse_scoring, data, history):
+        # note: the history fixture is required even if not used because it
+        # triggers the calls on mse_scoring
+        call_args_list = mse_scoring.pred_extractor.call_args_list
+        for (x, _), call_args in zip(data, call_args_list):
+            assert x == call_args[0][0]
