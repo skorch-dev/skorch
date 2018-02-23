3e028e254b6ef5588c98a6b46c6d25b83bb833d4
Felipe Ribeiro <felipelopes164@gmail.com>
2018-02-23 07:11:56 -0300

    Moved tests from setup_class to fixtures. Removed SchedulerTestNet.

--- skorch/tests/test_lr_scheduler.py
+++ skorch/tests/test_lr_scheduler.py
@@ -15,26 +15,9 @@ from skorch.net import NeuralNetClassifier
 from sklearn.datasets import make_classification
 from skorch.lr_scheduler import WarmRestartLR, LRScheduler
 
-class SchedulerTestNet(torch.nn.Module):
-    def __init__(self, num_units=10, nonlin=F.relu):
-        super(SchedulerTestNet, self).__init__()
-
-        self.dense0 = nn.Linear(20, num_units)
-        self.nonlin = nonlin
-        self.dropout = nn.Dropout(0.5)
-        self.dense1 = nn.Linear(num_units, 10)
-        self.output = nn.Linear(10, 2)
-
-    def forward(self, X):
-        X = self.nonlin(self.dense0(X))
-        X = self.dropout(X)
-        X = self.nonlin(self.dense1(X))
-        X = F.softmax(self.output(X), dim=-1)
-        return X
-
 class TestLRCallbacks:
 
-    def test_lr_callback_init_policies(self):
+    def test_lr_callback_init_policies(self, classifier_module):
         lr_scheduler_pack = [
             ('LambdaLR', LambdaLR, {'lr_lambda': (lambda x: 1e-1)}),
             ('StepLR', StepLR, {'step_size': 30}),
@@ -44,14 +27,21 @@ class TestLRCallbacks:
             ('WarmRestartLR', WarmRestartLR, {}),
         ]
         for policy, instance, kwargs in lr_scheduler_pack:
-            self._lr_callback_init_policies(policy, instance, **kwargs)
+            self._lr_callback_init_policies(
+                classifier_module(), policy, instance, **kwargs
+            )
 
-    def _lr_callback_init_policies(self, policy, instance, **kwargs):
+    def _lr_callback_init_policies(
+            self,
+            classifier_module,
+            policy, instance, 
+            **kwargs
+        ):
         X, y = make_classification(1000, 20, n_informative=10, random_state=0)
         X = X.astype(np.float32)
         lr_policy = LRScheduler(policy, **kwargs)
         net = NeuralNetClassifier(
-            SchedulerTestNet,  max_epochs=1, callbacks=[lr_policy]
+            classifier_module,  max_epochs=1, callbacks=[lr_policy]
         )
         net.fit(X, y)
         assert any(list(map(
@@ -61,11 +51,12 @@ class TestLRCallbacks:
         )))
 
 class TestWarmRestartLR():
-    def setup_class(self):
-        self.net = SchedulerTestNet()
-        self.opt = SGD(self.net.parameters(), lr = 0.05)
 
-    def test_warm_restart_lr(self):
+    @pytest.fixture()
+    def init_optimizer(self, classifier_module):
+        return SGD(classifier_module().parameters(), lr = 0.05)
+
+    def test_warm_restart_lr(self, init_optimizer):
         epochs = 10
         min_lr = 5e-5
         max_lr = 5e-2
@@ -76,23 +67,24 @@ class TestWarmRestartLR():
         single_targets = single_targets.tolist()
         targets = [single_targets, list(map(
             lambda x: x * epochs, single_targets))]
+        optimizer = init_optimizer
         scheduler = WarmRestartLR(
-            self.opt, min_lr, max_lr, base_period, period_mult
+            optimizer, min_lr, max_lr, base_period, period_mult
         )
-        self._test(scheduler, targets, epochs)
+        self._test(optimizer, scheduler, targets, epochs)
 
-    def _test(self, scheduler, targets, epochs=10):
+    def _test(self, optimizer, scheduler, targets, epochs=10):
         for epoch in range(epochs):
             scheduler.step(epoch)
-            for param_group, target in zip(self.opt.param_groups, targets):
+            for param_group, target in zip(optimizer.param_groups, targets):
                 assert param_group['lr'] == pytest.approx(target[epoch])
 
-    def test_raise_incompatible_len_on_min_lr_err(self):
+    def test_raise_incompatible_len_on_min_lr_err(self, init_optimizer):
         with pytest.raises(ValueError) as excinfo:
-            WarmRestartLR(self.opt, min_lr=[1e-1, 1e-2, 1e-3])
+            WarmRestartLR(init_optimizer, min_lr=[1e-1, 1e-2, 1e-3])
         assert 'min_lr' in str(excinfo.value)
 
-    def test_raise_incompatible_len_on_max_lr_err(self):
+    def test_raise_incompatible_len_on_max_lr_err(self, init_optimizer):
         with pytest.raises(ValueError) as excinfo:
-            WarmRestartLR(self.opt, max_lr=[1e-1, 1e-2, 1e-3])
+            WarmRestartLR(init_optimizer, max_lr=[1e-1, 1e-2, 1e-3])
         assert 'max_lr' in str(excinfo.value)
