b2f4507b216403ad3604e98e1c7b9af3257aea3c
Scott Sievert <github@stsievert.com>
2018-05-07 14:04:05 -0500

    MAINT: enforce torch versions

--- skorch/__init__.py
+++ skorch/__init__.py
@@ -1,6 +1,22 @@
 """skorch base imports"""
 
 import pkg_resources
+from pkg_resources import parse_version
+
+MIN_TORCH_VERSION = '0.4.0'
+
+try:
+    import torch
+except:  # pylint: disable=bare-except
+    raise Exception('skorch depends on PyTorch. Visit https://pytorch.org/ '
+                    'for install instructions')
+
+torch_version = pkg_resources.get_distribution('torch').version
+if parse_version(torch_version) < parse_version(MIN_TORCH_VERSION):
+    msg = ('skorch depends a newer version of PyTorch (at least {req}, not '
+           '{installed}). Visit https://pytorch.org for install details')
+    raise Exception(msg.format(req=MIN_TORCH_VERSION, installed=torch_version))
+
 
 from .history import History
 from .net import NeuralNet
