103163ebe62329ae4f57ea14a8db800dc16a2bb5
Tomasz Pietruszka <tomasz.pietruszka@predictail.com>
2018-06-10 17:46:41 +0200

    Added tests for EarlyStoppingCallback

--- skorch/tests/callbacks/test_early_stopping.py
+++ skorch/tests/callbacks/test_early_stopping.py
@@ -12,6 +12,11 @@ class TestEarlyStopping:
         return EarlyStopping
 
     @pytest.fixture
+    def epoch_scoring_cls(self):
+        from skorch.callbacks import EpochScoring
+        return EpochScoring
+
+    @pytest.fixture
     def net_clf_cls(self):
         from skorch import NeuralNetClassifier
         return NeuralNetClassifier
@@ -25,11 +30,11 @@ class TestEarlyStopping:
         return BrokenClassifier
 
     def test_typical_use_case_nonstop(
-            self, net_clf_cls, classifier_module, classifier_data, early_stopping_cls,
-    ):
-        stop_threshold = 5
+            self, net_clf_cls, classifier_module, classifier_data,
+            early_stopping_cls):
+        patience = 5
         max_epochs = 8
-        early_stopping_cb = early_stopping_cls(patience=stop_threshold)
+        early_stopping_cb = early_stopping_cls(patience=patience)
 
         net = net_clf_cls(
             classifier_module,
@@ -46,11 +51,11 @@ class TestEarlyStopping:
         assert ok_run
 
     def test_typical_use_case_stopping(
-            self, net_clf_cls, broken_classifier_module, classifier_data, early_stopping_cls,
-    ):
-        stop_threshold = 5
+            self, net_clf_cls, broken_classifier_module, classifier_data,
+            early_stopping_cls):
+        patience = 5
         max_epochs = 8
-        early_stopping_cb = early_stopping_cls(patience=stop_threshold)
+        early_stopping_cb = early_stopping_cls(patience=patience)
 
         net = net_clf_cls(
             broken_classifier_module,
@@ -67,5 +72,99 @@ class TestEarlyStopping:
 
         assert bad_run
 
+    def test_custom_scoring_nonstop(
+        self, net_clf_cls, classifier_module, classifier_data,
+            early_stopping_cls, epoch_scoring_cls):
+        lower_is_better = False
+        scoring_name = 'valid_roc_auc'
+        patience = 5
+        max_epochs = 8
+        scoring_cb = epoch_scoring_cls(
+            'roc_auc', lower_is_better, name=scoring_name)
+        early_stopping_cb = early_stopping_cls(
+            patience=patience, lower_is_better=lower_is_better,
+            monitor=scoring_name)
 
+        net = net_clf_cls(
+            classifier_module,
+            callbacks=[
+                scoring_cb,
+                early_stopping_cb,
+            ],
+            max_epochs=max_epochs,
+        )
+        net.fit(*classifier_data)
 
+        ok_run = (
+                len(net.history) == max_epochs
+        )
+        assert ok_run
+
+    def test_custom_scoring_stop(
+        self, net_clf_cls, broken_classifier_module, classifier_data,
+            early_stopping_cls, epoch_scoring_cls):
+        lower_is_better = False
+        scoring_name = 'valid_roc_auc'
+        patience = 5
+        max_epochs = 8
+        scoring_cb = epoch_scoring_cls(
+            'roc_auc', lower_is_better, name=scoring_name)
+        early_stopping_cb = early_stopping_cls(
+            patience=patience, lower_is_better=lower_is_better,
+            monitor=scoring_name)
+
+        net = net_clf_cls(
+            broken_classifier_module,
+            callbacks=[
+                scoring_cb,
+                early_stopping_cb,
+            ],
+            max_epochs=max_epochs,
+        )
+        net.fit(*classifier_data)
+
+        stopped_run = (
+                len(net.history) != max_epochs
+        )
+        assert stopped_run
+
+    def test_stopping_big_absolute_threshold(
+            self, net_clf_cls, classifier_module, classifier_data,
+            early_stopping_cls):
+        patience = 5
+        max_epochs = 8
+        early_stopping_cb = early_stopping_cls(patience=patience,
+                                               threshold_mode='abs',
+                                               threshold=0.1)
+
+        net = net_clf_cls(
+            classifier_module,
+            callbacks=[
+                early_stopping_cb,
+            ],
+            max_epochs=max_epochs,
+        )
+        net.fit(*classifier_data)
+
+        stopped_run = (
+            len(net.history) != max_epochs
+        )
+        assert stopped_run
+
+    def test_wrong_threshold_mode(
+            self, net_clf_cls, classifier_module, classifier_data,
+            early_stopping_cls):
+        with pytest.raises(ValueError) as e_info:
+            patience = 5
+            max_epochs = 8
+            early_stopping_cb = early_stopping_cls\
+                (patience=patience, threshold_mode='incorrect')
+
+            net = net_clf_cls(
+                classifier_module,
+                callbacks=[
+                    early_stopping_cb,
+                ],
+                max_epochs=max_epochs,
+            )
+            net.fit(*classifier_data)
