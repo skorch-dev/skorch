98d0ce0fed6f11f0b455eaea07e6323c85254b35
Thomas Fan <thomasjpfan@gmail.com>
2018-07-02 03:54:27 -0400

    ENH: Adds NeuralNetBinaryClassifier (#239)
    * API: Adds NeuralNetBinaryClassifier
    
    * RFC: Changes subclass to NeuralNet
    
    * STY: Python 3 does not need args in super
    
    * REV: Remove fix to keep PR simplier
    
    * RFC: Moves threshold to init
    
    * RFC: Test
    
    * ENH: Adds check for BCEWithLogitsLoss
    
    * TST: Test for loss function
    
    * TST: Refactor
    
    * RFC: Moves args

--- skorch/tests/test_classifier.py
+++ skorch/tests/test_classifier.py
@@ -117,3 +117,118 @@ class TestNeuralNet:
         for (y_out, _), _ in mock_loss.call_args_list:
             assert not (y_out < 0).all()
             assert torch.isclose(torch.ones(len(y_out)), y_out.sum(1)).all()
+
+
+class MyBinaryClassifier(nn.Module):
+    """Simple binary classification module.
+
+    We cannot use the module fixtures from conftest because they are
+    not pickleable.
+
+    """
+
+    def __init__(self, num_units=10, nonlin=F.relu):
+        super().__init__()
+
+        self.dense0 = nn.Linear(20, num_units)
+        self.nonlin = nonlin
+        self.dropout = nn.Dropout(0.5)
+        self.dense1 = nn.Linear(num_units, 10)
+        self.output = nn.Linear(10, 1)
+
+    # pylint: disable=arguments-differ
+    def forward(self, X):
+        X = self.nonlin(self.dense0(X))
+        X = self.dropout(X)
+        X = self.nonlin(self.dense1(X))
+        X = self.output(X).squeeze_(-1)
+        return X
+
+
+class TestNeuralNetBinaryClassifier:
+    @pytest.fixture(scope='module')
+    def data(self, classifier_data):
+        X, y = classifier_data
+        return X, y.astype('float32')
+
+    @pytest.fixture(scope='module')
+    def module_cls(self):
+        return MyBinaryClassifier
+
+    @pytest.fixture(scope='module')
+    def net_cls(self):
+        from skorch.classifier import NeuralNetBinaryClassifier
+        return NeuralNetBinaryClassifier
+
+    @pytest.fixture(scope='module')
+    def net(self, net_cls, module_cls):
+        return net_cls(
+            module_cls,
+            max_epochs=1,
+            lr=0.1,
+        )
+
+    @pytest.fixture(scope='module')
+    def net_fit(self, net, data):
+        # Careful, don't call additional fits on this, since that would have
+        # side effects on other tests.
+        X, y = data
+        return net.fit(X, y)
+
+    def test_fit(self, net_fit):
+        # fitting does not raise anything
+        pass
+
+    def test_history_default_keys(self, net_fit):
+        expected_keys = {
+            'train_loss', 'valid_loss', 'epoch', 'dur', 'batches', 'valid_acc'
+        }
+        for row in net_fit.history:
+            assert expected_keys.issubset(row)
+
+    @pytest.mark.parametrize('threshold', [0, 0.25, 0.5, 0.75, 1])
+    def test_predict_predict_proba(self, net, data, threshold):
+        X, y = data
+        net.threshold = threshold
+        net.fit(X, y)
+
+        y_pred_proba = net.predict_proba(X)
+        assert y_pred_proba.ndim == 1
+        assert y_pred_proba.shape[0] == X.shape[0]
+
+        y_pred_exp = (y_pred_proba > threshold).astype('uint8')
+
+        y_pred_actual = net.predict(X)
+        assert np.allclose(y_pred_exp, y_pred_actual)
+
+    def test_target_2d_raises(self, net, data):
+        X, y = data
+        with pytest.raises(ValueError) as exc:
+            net.fit(X, y[:, None])
+
+        assert exc.value.args[0] == (
+            "The target data should be 1-dimensional.")
+
+    def test_custom_loss_does_not_call_sigmoid(
+            self, net_cls, data, module_cls, monkeypatch):
+        mock = Mock(side_effect=lambda x: x)
+        monkeypatch.setattr(torch.nn.functional, "sigmoid", mock)
+
+        net = net_cls(module_cls, max_epochs=1, lr=0.1, criterion=nn.MSELoss)
+        X, y = data
+        net.fit(X, y)
+
+        net.predict_proba(X)
+        assert mock.call_count == 0
+
+    def test_default_loss_does_call_sigmoid(
+            self, net_cls, data, module_cls, monkeypatch):
+        mock = Mock(side_effect=lambda x: x)
+        monkeypatch.setattr(torch.nn.functional, "sigmoid", mock)
+
+        net = net_cls(module_cls, max_epochs=1, lr=0.1)
+        X, y = data
+        net.fit(X, y)
+
+        net.predict_proba(X)
+        assert mock.call_count > 0
