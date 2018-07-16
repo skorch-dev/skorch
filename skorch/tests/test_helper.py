8b16c41307427176f7bc3f8e268a34e7d69739d2
Thomas Fan <thomasjpfan@gmail.com>
2018-07-16 13:59:17 -0400

    BUG: Adds pickle support to filtered_optimizer

--- skorch/tests/test_helper.py
+++ skorch/tests/test_helper.py
@@ -1,4 +1,5 @@
 """Test for helper.py"""
+import pickle
 
 import numpy as np
 import pytest
@@ -280,3 +281,11 @@ class TestOptimizerParamsRequiresGrad:
         assert isinstance(net.optimizer_, torch.optim.SGD)
         assert len(net.optimizer_.param_groups) == 1
         assert net.optimizer_.param_groups[0]['momentum'] == 0.9
+
+    def test_pickle(self, filtered_optimizer, filter_requires_grad, tmpdir):
+        opt_fn = tmpdir.mkdir('filtered_optiizer').join('opt.pkl')
+        opt = filtered_optimizer(torch.optim.SGD, filter_requires_grad)
+
+        # Does not raise
+        with opt_fn.open('wb') as f:
+            pickle.dump(opt, f)
