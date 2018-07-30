1ba2363f107c87bc4d661ab74619f209b5d4f8f5
Andrew Spott <andrew.spott@gmail.com>
2018-07-30 15:43:56 -0600

    Fixing broken tests:  callback arguments must have a default value

--- skorch/callbacks/base.py
+++ skorch/callbacks/base.py
@@ -28,31 +28,39 @@ class Callback:
         """
         return self
 
-    def on_train_begin(self, net, X, y, **kwargs):
+    def on_train_begin(self, net,
+                       X=None, y=None, **kwargs):
         """Called at the beginning of training."""
         pass
 
-    def on_train_end(self, net, X, y, **kwargs):
+    def on_train_end(self, net,
+                     X=None, y=None, **kwargs):
         """Called at the end of training."""
         pass
 
-    def on_epoch_begin(self, net, dataset_train, dataset_valid, **kwargs):
+    def on_epoch_begin(self, net,
+                       dataset_train=None, dataset_valid=None, **kwargs):
         """Called at the beginning of each epoch."""
         pass
 
-    def on_epoch_end(self, net, dataset_train, dataset_valid,  **kwargs):
+    def on_epoch_end(self, net,
+                     dataset_train=None, dataset_valid=None, **kwargs):
         """Called at the end of each epoch."""
         pass
 
-    def on_batch_begin(self, net, Xi, yi, training, **kwargs):
+    def on_batch_begin(self, net,
+                       Xi=None, yi=None, training=None, **kwargs):
         """Called at the beginning of each batch."""
         pass
 
-    def on_batch_end(self, net, Xi, yi, training, loss, y_pred, **kwargs):
+    def on_batch_end(self, net,
+                     Xi=None, yi=None, training=None, loss=None, y_pred=None,
+                     **kwargs):
         """Called at the end of each batch."""
         pass
 
-    def on_grad_computed(self, net, named_parameters, Xi, yi, training, **kwargs):
+    def on_grad_computed(self, net, named_parameters,
+                         Xi=None, yi=None, training=None, **kwargs):
         """Called once per batch after gradients have been computed but before
         an update step was performed.
         """
