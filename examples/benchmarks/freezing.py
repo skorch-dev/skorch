ee4e644bfaa37ebaeb510ab0232dc61990cb4399
Marian Tietz <marian.tietz@ottogroup.com>
2018-09-18 10:39:12 +0200

    Use toy module in freezing benchmark and use fork
    The toy module saves dummy module code while forking
    each test ensures that each test has its own resource
    pool which makes comparing resource usage (i.e. memory)
    easier and more reliable.

--- examples/benchmarks/freezing.py
+++ examples/benchmarks/freezing.py
@@ -11,39 +11,39 @@ explicitly removing the parameters from the optimizer.
 In theory there should be no difference in memory
 consumption and runtime.
 """
+from functools import partial
+import resource
+from multiprocessing import Process, Queue
+
 import torch
 import skorch
 import skorch.helper
+from skorch.toy import make_classifier
 import sklearn.datasets
 import numpy as np
-import resource
-
-class MyModule(torch.nn.Module):
-    def __init__(self, input_dim=2, hidden_dim=100, output_dim=2, n_layers=2):
-        super().__init__()
-        d = hidden_dim
-        layers = [torch.nn.Linear(input_dim, d)]
-        layers += [torch.nn.Linear(d, d) for _ in range(n_layers)]
-        layers += [torch.nn.Linear(d, output_dim)]
-        self.seq = torch.nn.Sequential(*layers)
-
-    def forward(self, X):
-        return torch.softmax(self.seq(X), dim=-1)
 
 
 X, y = sklearn.datasets.make_classification(
-        n_samples=1000,
-        n_features=2,
-        n_informative=2,
-        n_redundant=0,
-        n_classes=2,
-        random_state=0)
+    n_samples=1000,
+    n_features=2,
+    n_informative=2,
+    n_redundant=0,
+    n_classes=2,
+    random_state=0)
 X = X.astype('float32')
 y = y.astype('int64')
 
 N_LAYERS = 2
 
+make_module_cls = partial(
+    make_classifier,
+    num_hidden=N_LAYERS,
+    input_units=2,
+    hidden_units=100,
+    output_units=2,
+)
 
+linear_idcs = list(range(0, (N_LAYERS+1)*3, 3))
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -55,31 +55,31 @@ def test_a():
     torch.manual_seed(0)
 
     print('1', end='')
-    mod = MyModule(n_layers=N_LAYERS)
+    mod = make_module_cls()()
 
     # freeze all params but last layer
-    for i in range(len(mod.seq) - 1):
-        skorch.utils.freeze_parameter(mod.seq[i].weight)
-        skorch.utils.freeze_parameter(mod.seq[i].bias)
+    for i in linear_idcs[:-1]:
+        skorch.utils.freeze_parameter(mod.sequential[i].weight)
+        skorch.utils.freeze_parameter(mod.sequential[i].bias)
 
     opt = skorch.helper.filtered_optimizer(
-            torch.optim.SGD,
-            skorch.helper.filter_requires_grad)
+        torch.optim.SGD,
+        skorch.helper.filter_requires_grad)
 
     net = skorch.NeuralNetClassifier(
-            mod,
-            verbose=0,
-            optimizer=opt,
-            warm_start=True)
+        mod,
+        verbose=0,
+        optimizer=opt,
+        warm_start=True)
 
-    for i in range(len(mod.seq) - 1):
-        assert not mod.seq[i].weight.requires_grad
+    for i in linear_idcs[:-1]:
+        assert not mod.sequential[i].weight.requires_grad
 
     net.fit(X, y)
 
-    for i in range(len(mod.seq) - 1):
-        assert not mod.seq[i].weight.requires_grad
-        assert not net.module_.seq[i].weight.requires_grad
+    for i in linear_idcs[:-1]:
+        assert not mod.sequential[i].weight.requires_grad
+        assert not net.module_.sequential[i].weight.requires_grad
 
     rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     return rss, net.history[-1, 'valid_loss'], np.mean(net.history[:, 'dur'])
@@ -91,36 +91,49 @@ def test_b(verbose=False):
     torch.manual_seed(0)
 
     print('2', end='')
-    mod = MyModule(n_layers=N_LAYERS)
+    mod = make_module_cls()()
 
     opt = torch.optim.SGD
     cb = skorch.callbacks.Freezer(
-        ['seq.{}.weight'.format(i) for i in range(len(mod.seq) - 1)] +
-        ['seq.{}.bias'.format(i) for i in range(len(mod.seq) - 1)]
+        ['sequential.{}.weight'.format(i) for i in linear_idcs[:-1]] +
+        ['sequential.{}.bias'.format(i) for i in linear_idcs[:-1]]
     )
 
     net = skorch.NeuralNetClassifier(
-            mod,
-            verbose=0,
-            optimizer=opt,
-            callbacks=[cb])
+        mod,
+        verbose=0,
+        optimizer=opt,
+        callbacks=[cb])
     net.fit(X, y)
 
-    for i in range(len(mod.seq) - 1):
-        assert not mod.seq[i].weight.requires_grad
-        assert not net.module_.seq[i].weight.requires_grad
+    for i in linear_idcs[:-1]:
+        assert not mod.sequential[i].weight.requires_grad
+        assert not net.module_.sequential[i].weight.requires_grad
 
     rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     return rss, net.history[-1, 'valid_loss'], np.mean(net.history[:, 'dur'])
 
+def test_runner(q, fn, n_runs):
+    q.put(np.mean([fn() for _ in range(n_runs)], axis=0))
+
+def test_forker(test_fn, n_runs):
+    q = Queue()
+    p = Process(target=test_runner, args=(q, test_fn, n_runs))
+    p.start()
+    res = q.get()
+    p.join()
+    return res
 
 if __name__ == '__main__':
     n_runs = 10
     print(f'running tests for {n_runs} runs each.')
 
-    dur_a = np.mean([test_a() for _ in range(n_runs)], axis=0)
-    dur_b = np.mean([test_b() for _ in range(n_runs)], axis=0)
+    # We fork the tests so that each has its own process and thus its
+    # own memory allocation. Therefore tests don't influence each other.
+    dur_a = test_forker(test_a, n_runs)
+    print()
 
+    dur_b = test_forker(test_b, n_runs)
     print()
 
     print(f'test_a: µ_rss = {dur_a[0]}, µ_valid_loss = {dur_a[1]}, µ_dur={dur_a[2]}')
@@ -130,7 +143,7 @@ if __name__ == '__main__':
     assert np.allclose(dur_a[1], dur_b[1])
 
     # memory usage should be nearly identical (within 4MiB)
-    assert abs(dur_a[0] - dur_b[0]) < (4*1024**2)
+    assert np.allclose(dur_a[0], dur_b[0], atol=4*1024**2)
 
     # duration should be nearly identical
-    assert abs(dur_a[2] - dur_b[2]) < 1e-3
+    assert np.allclose(dur_a[2], dur_b[2], atol=0.5)
