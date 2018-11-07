76eecdd722ee0b64140e4ce8b21f821209e2c367
Thomas Fan <thomasjpfan@gmail.com>
2018-11-07 10:01:02 -0500

    RFC: Adds usage details

--- skorch/utils.py
+++ skorch/utils.py
@@ -471,7 +471,8 @@ def get_map_location(target_device, fallback_device='cpu'):
 
 class TeeGenerator:
     """Stores a generator and calls ``tee`` on it to create new generators
-    when ``TeeGenerator`` is iterated over.
+    when ``TeeGenerator`` is iterated over to let you iterate over the given
+    generator more than once.
 
     """
     def __init__(self, gen):
