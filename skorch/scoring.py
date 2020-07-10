add: sample_weight argument, reduction tests. correct averaging.

skorch/scoring.py
@@ -3,7 +3,7 @@ from skorch.net import NeuralNet
 from skorch.dataset import unpack_data
 
 
-def loss_scoring(net: NeuralNet, X, y=None):
+def loss_scoring(net: NeuralNet, X, y=None, sample_weight=None):
     """Calculate score using the criterion of the net
     
     Use the exact same logic as during model training to calculate the score.
@@ -38,6 +38,9 @@ def loss_scoring(net: NeuralNet, X, y=None):
         The same data types as for ``X`` are supported. If your X is a Dataset
         that contains the target, ``y`` may be set to None.
 
+    sample_weight : array-like of shape (n_samples,)
+        Sample weights.
+
     Returns
     -------
     loss_value : float32 or np.ndarray
@@ -46,12 +49,21 @@ def loss_scoring(net: NeuralNet, X, y=None):
         this function returns a ``np.ndarray`` object.
 
     """
+    if sample_weight is not None:
+        raise NotImplementedError(
+            "sample_weight for loss_scoring is not yet supported."
+        )
+
     net.check_is_fitted()
 
     dataset = net.get_dataset(X, y)
     iterator = net.get_iterator(dataset, training=False)
     history = {"loss": [], "batch_size": []}
     reduction = net.criterion_.reduction
+    assert reduction in ["mean", "sum", "none",], (
+        "Expected one of 'mean', 'sum' or 'none' "
+        f"for reduction but got {reduction}."
+    )
     for data in iterator:
         Xi, yi = unpack_data(data)
         yp = net.evaluation_step(Xi, training=False)
@@ -65,7 +77,6 @@ def loss_scoring(net: NeuralNet, X, y=None):
 
     if reduction == "none":
         return np.concatenate(history["loss"], 0)
-    loss_value = np.average(history["loss"], weights=history["batch_size"])
     if reduction == "sum":
-        loss_value *= len(history["batch_size"])
-    return loss_value
+        return np.sum(history["loss"])
+    return np.average(history["loss"], weights=history["batch_size"])

skorch/tests/test_scoring.py
@@ -8,6 +8,10 @@ class TestLossScoring:
     def data(self, classifier_data):
         return classifier_data
 
+    @pytest.fixture(scope="module", params=["mean", "sum"])
+    def reduction(self, request):
+        return request.param
+
     @pytest.fixture(scope="module")
     def net_cls(self):
         from skorch import NeuralNetClassifier
@@ -19,8 +23,8 @@ class TestLossScoring:
         return classifier_module
 
     @pytest.fixture(scope="module")
-    def net(self, net_cls, module_cls):
-        return net_cls(module_cls, lr=0.1)
+    def net(self, net_cls, module_cls, reduction):
+        return net_cls(module_cls, lr=0.1, criterion__reduction=reduction)
 
     @pytest.fixture(scope="module")
     def net_fit(self, net, data):
@@ -42,8 +46,10 @@ class TestLossScoring:
         return ScoredNet
 
     @pytest.fixture(scope="module")
-    def scored_net(self, scored_net_cls, module_cls):
-        return scored_net_cls(module_cls, lr=0.01)
+    def scored_net(self, scored_net_cls, module_cls, reduction):
+        return scored_net_cls(
+            module_cls, lr=0.01, criterion__reduction=reduction
+        )
 
     @pytest.fixture(scope="module")
     def scored_net_fit(self, scored_net, data):
@@ -64,6 +70,13 @@ class TestLossScoring:
         with pytest.raises(NotInitializedError):
             scored_net.score(X, y)
 
+    def test_nonnull_sample_weight_raises(self, loss_scoring_fn, net_fit, data):
+        X, y = data
+        with pytest.raises(NotImplementedError):
+            loss_scoring_fn(
+                net_fit, X, y, sample_weight=np.random.rand(X.shape[0])
+            )
+
     def test_scored_net_output_type(self, scored_net_fit, data):
         X, y = data
         score_value = scored_net_fit.score(X, y)
@@ -76,9 +89,26 @@ class TestLossScoring:
 
     def test_scored_net_matches_criterion_value(self, scored_net_fit, data):
         X, y = data
-        score_value = scored_net_fit.score(X, y)
-        criterion = scored_net_fit.criterion_
-        y = torch.as_tensor(y).long()
         y_val_proba = torch.as_tensor(scored_net_fit.predict_proba(X))
-        loss_value = criterion(y_val_proba, y)
-        assert score_value == loss_value.item()
+        loss_value = scored_net_fit.get_loss(y_val_proba, y)
+        score_value = scored_net_fit.score(X, y)
+        assert np.allclose(score_value, loss_value.item())
+
+    def test_scored_net_with_reduction_none(
+        self, scored_net_cls, module_cls, reduction, data
+    ):
+        X, y = data
+        net = scored_net_cls(
+            module_cls, lr=0.01, criterion__reduction=reduction
+        ).fit(X, y)
+        net.set_params(criterion__reduction="none")
+        output = net.score(X, y)
+        assert output.shape[0] == X.shape[0]
+
+    def test_score_unknown_reduction_raises(
+        self, loss_scoring_fn, net_fit, data
+    ):
+        X, y = data
+        net_fit.set_params(criterion__reduction="unk")
+        with pytest.raises(AssertionError):
+            loss_scoring_fn(net_fit, X, y)