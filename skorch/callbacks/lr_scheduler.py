ba35743d4101151591a67ddb9358d343da7cf09a
BenjaminBossan <b.bossan@gmail.com>
2020-08-30 13:00:10 +0200

    Remove a FutureWarning in LRScheduler

--- skorch/callbacks/lr_scheduler.py
+++ skorch/callbacks/lr_scheduler.py
@@ -115,16 +115,6 @@ class LRScheduler(Callback):
         self.policy_ = self._get_policy_cls()
         self.lr_scheduler_ = None
         self.batch_idx_ = 0
-        # TODO: Remove this warning on 0.10 release
-        if (self.policy_ == TorchCyclicLR or self.policy_ == "TorchCyclicLR"
-                and self.step_every == 'epoch'):
-            warnings.warn(
-                "The LRScheduler now makes a step every epoch by default. "
-                "To have the cyclic lr scheduler update "
-                "every batch set step_every='batch'",
-                FutureWarning
-            )
-
         return self
 
     def _get_policy_cls(self):
