297349d8ed473e4b63890cddb09d939fa2ee3551
Timo Kaufmann <timokau@zoho.com>
2022-05-24 20:34:22 +0200

    NotInitializedError inherits from NotFittedError (#860)
    This ensures that a user can catch the sklearn `NotFittedError`, just as
    they can with regular sklearn estimators. As discussed in [1].
    
    [1] https://github.com/skorch-dev/skorch/issues/859

--- skorch/exceptions.py
+++ skorch/exceptions.py
@@ -1,11 +1,13 @@
 """Contains skorch-specific exceptions and warnings."""
 
+from sklearn.exceptions import NotFittedError
+
 
 class SkorchException(BaseException):
     """Base skorch exception."""
 
 
-class NotInitializedError(SkorchException):
+class NotInitializedError(SkorchException, NotFittedError):
     """Module is not initialized, please call the ``.initialize``
     method or train the model by calling ``.fit(...)``.
 
