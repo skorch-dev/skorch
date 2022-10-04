85a6994b48cb34715e98502b4ee49b0ecdac6894
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:24:20 +0000

    Adding numeric_owner as keyword arguement

--- examples/rnn_classifer/model.py
+++ examples/rnn_classifer/model.py
@@ -42,7 +42,7 @@ def download():
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(f)
