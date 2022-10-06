81435a10ee6c6d2c88363c19549944a269bc8420
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-10-06 06:25:47 -0500

    CVE-2007-4559 Patch (#899)
    Adding tarfile member sanitization to extractall()

--- examples/rnn_classifer/model.py
+++ examples/rnn_classifer/model.py
@@ -26,7 +26,24 @@ def download():
         if not os.path.exists(DATA_FN):
             urlretrieve(DATA_URL, DATA_FN)
         with tarfile.open(DATA_FN, 'r:gz') as f:
-            f.extractall()
+            def is_within_directory(directory, target):
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                # See:
+                # https://docs.python.org/3/library/tarfile.html#tarfile.TarFile.extractall
+                # on why plain tar.extractall can be dangerous
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise OSError("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+            safe_extract(f)
 
 
 class DatasetLoader(IDatasetLoader):
