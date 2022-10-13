014d03dc06258bba398de1d49ea31e93e86eae19
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2022-10-13 11:28:53 +0200

    Update link with an example for Neptune integration

--- skorch/callbacks/logging.py
+++ skorch/callbacks/logging.py
@@ -81,7 +81,7 @@ class NeptuneLogger(Callback):
     $ python -m pip install psutil
 
     You can view example experiment logs here:
-    https://ui.neptune.ai/o/shared/org/skorch-integration/e/SKOR-13/charts
+    https://app.neptune.ai/shared/skorch-integration/e/SKOR-23/
 
     Examples
     --------
