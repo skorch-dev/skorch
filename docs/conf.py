c5d563e9b95bdf3df09bd196f095d6eeba592641
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-08-31 12:43:44 +0200

    Attempt at fixing theme on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -126,7 +126,7 @@ def setup(app):
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if True or not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
