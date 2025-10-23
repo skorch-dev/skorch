038c7f84b1c4e43c68882f6884e80206b64c3b4a
Benjamin Bossan <BenjaminBossan@users.noreply.github.com>
2025-10-23 18:16:01 +0200

    Update Python (3.13) and PyTorch (2.9) versions (#1121)
    Also:
    
    - drop Python 3.9 (EOL)
    - drop PyTorch 2.5
    - update some tests
    - for torch < 2.9, downgrade to triton 3.4
    - fix indentation error in GP classes docstrings (similar to #1080)

--- skorch/tests/test_net.py
+++ skorch/tests/test_net.py
@@ -511,16 +511,11 @@ class TestNeuralNet:
             # 1. one for the failed load
             # 2. for switching devices on the net instance
             # remove possible future warning about weights_only=False
-            # TODO: remove filter when torch<=2.4 is dropped
-            w_list = [
-                warning for warning in w.list
-                if "weights_only=False" not in warning.message.args[0]
-            ]
-            assert len(w_list) == 2
-            assert w_list[0].message.args[0] == (
+            assert len(w.list) == 2
+            assert w.list[0].message.args[0] == (
                 'Requested to load data to CUDA but no CUDA devices '
                 'are available. Loading on device "cpu" instead.')
-            assert w_list[1].message.args[0] == (
+            assert w.list[1].message.args[0] == (
                 'Setting self.device = {} since the requested device ({}) '
                 'is not available.'.format(load_dev, save_dev))
 
@@ -3085,46 +3080,10 @@ class TestNeuralNet:
         del call_kwargs['map_location']  # we're not interested in that
         assert call_kwargs == expected_kwargs
 
-    def test_torch_load_kwargs_auto_weights_false_pytorch_lt_2_6(
+    def test_torch_load_kwargs_auto_weights_true(
             self, net_cls, module_cls, monkeypatch, tmp_path
     ):
-        # Same test as
-        # test_torch_load_kwargs_auto_weights_only_false_when_load_params but
-        # without monkeypatching get_default_torch_load_kwargs. The default is
-        # weights_only=False.
         # See discussion in 1063.
-        from skorch._version import Version
-
-        # TODO remove once torch 2.5.0 is no longer supported
-        if Version(torch.__version__) >= Version('2.6.0'):
-            pytest.skip("Test only for torch < v2.6.0")
-
-        net = net_cls(module_cls).initialize()
-        net.save_params(f_params=tmp_path / 'params.pkl')
-        state_dict = net.module_.state_dict()
-        expected_kwargs = {"weights_only": False}
-
-        mock_torch_load = Mock(return_value=state_dict)
-        monkeypatch.setattr(torch, "load", mock_torch_load)
-        net.load_params(f_params=tmp_path / 'params.pkl')
-
-        call_kwargs = mock_torch_load.call_args_list[0].kwargs
-        del call_kwargs['map_location']  # we're not interested in that
-        assert call_kwargs == expected_kwargs
-
-    def test_torch_load_kwargs_auto_weights_true_pytorch_ge_2_6(
-            self, net_cls, module_cls, monkeypatch, tmp_path
-    ):
-        # Same test as
-        # test_torch_load_kwargs_auto_weights_false_pytorch_lt_2_6 but
-        # with weights_only=True, since it's the new default
-        # See discussion in 1063.
-        from skorch._version import Version
-
-        # TODO remove once torch 2.5.0 is no longer supported
-        if Version(torch.__version__) < Version('2.6.0'):
-            pytest.skip("Test only for torch >= 2.6.0")
-
         net = net_cls(module_cls).initialize()
         net.save_params(f_params=tmp_path / 'params.pkl')
         state_dict = net.module_.state_dict()
@@ -4332,16 +4291,6 @@ class TestTorchCompile:
             ).initialize()
 
     def test_fit_and_predict_with_compile(self, net_cls, module_cls, data):
-        if not hasattr(torch, 'compile'):
-            pytest.skip(reason="torch.compile not available")
-
-        # python 3.12 requires torch >= 2.4 to support compile
-        # TODO: remove once we remove support for torch < 2.4
-        from skorch._version import Version
-
-        if Version(torch.__version__) < Version('2.4.0') and sys.version_info >= (3, 12):
-            pytest.skip(reason="When using Python 3.12, torch.compile requires torch >= 2.4")
-
         # use real torch.compile, not mocked, can be a bit slow
         X, y = data
         net = net_cls(module_cls, max_epochs=1, compile=True).initialize()
@@ -4361,13 +4310,6 @@ class TestTorchCompile:
         # resulting in _infer_predict_nonlinearity to return the wrong result
         # because of a failing isinstance check
         from skorch import NeuralNetBinaryClassifier
-
-        # python 3.12 requires torch >= 2.4 to support compile
-        # TODO: remove once we remove support for torch < 2.4
-        from skorch._version import Version
-
-        if Version(torch.__version__) < Version('2.4.0') and sys.version_info >= (3, 12):
-            pytest.skip(reason="When using Python 3.12, torch.compile requires torch >= 2.4")
 
         X, y = data[0], data[1].astype(np.float32)
 
